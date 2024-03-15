9b75925d368a4b428183acde445c45e3206214e4
Siddarthg-Dev <siddarthg.dev@gmail.com>
2024-03-15 17:31:24 +0530

    Add Components and screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationsComponent } from './components/navigations/navigations.component';
+import { HomeComponent } from './screens/home/home.component';
+import { ShopComponent } from './screens/shop/shop.component';
+import { BlogComponent } from './screens/blog/blog.component';
+import { AboutusComponent } from './screens/aboutus/aboutus.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavigationsComponent, HomeComponent, ShopComponent, BlogComponent, AboutusComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
